2425ee5e021e648fa70e5ea21612d5e9890e3add
Waveyyyy <Waves-sec@protonmail.com>
2023-04-17 21:53:26 +0100

    Added basic argument parsing
    One required argument:
    - sample -- the file which is to be analysed
    
    One optional argument:
    - -y / --yes -- any yes/no questions requiring user action will be
      answered with yes.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import argparse
 import r2pipe
 
 
@@ -8,8 +9,19 @@ class Analysis():
     def __init__(self):
         pass
 
-    def main():
-        pass
+    def argParser(self):
+        '''Handles argument parsing using the argparse module'''
+        parser = argparse.ArgumentParser(
+            description="Initial malware triage automation")
+        parser.add_argument("sample", help="The file to analyse")
+        parser.add_argument("-y", "--yes", action='store_true',
+                            help="Answer yes to any y/n prompts")
+
+        self.args = parser.parse_args()
+
+    def main(self):
+        '''Execution flow starts here'''
+        self.argParser()
 
 
 if __name__ == "__main__":
