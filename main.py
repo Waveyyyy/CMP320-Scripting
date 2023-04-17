df870e6d2b7941cc7f13a88a71c9e0ee06a8a07f
Waveyyyy <Waves-sec@protonmail.com>
2023-04-17 18:46:51 +0100

    Main.py created with placeholder code
    Main.py will be the only file for this script and therefore will make
    use of classes and methods to separate code nicely. (will see how well
    this goes lol)
    
    requirements.txt will contain the most up-to-date list of modules used
    by the script, as of right now this is:
    - r2pipe

---
+++ main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import r2pipe
+
+
+class Analysis():
+
+    def __init__(self):
+        pass
+
+    def main():
+        pass
+
+
+if __name__ == "__main__":
+    analyse = Analysis()
+    analyse.main()
